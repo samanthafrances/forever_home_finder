275bcec2ce07e6f66752f89c097be7d133a7066f
Emmy <emilyileeva@gmail.com>
2024-04-19 11:50:31 -0700

    add to navbar, create blog, donate, and volunteer

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('volunteer/', views.volunteer_view, name='volunteer'),
+    path('blog/', views.blog_view, name='blog'),
+    path('donate/', views.donate_view, name='donate'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,12 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def volunteer_view(request):
+    return render(request, 'volunteer.html')
+
+def blog_view(request):
+    return render(request, 'blog.html')
+
+def donate_view(request):
+    return render(request, 'donate.html')
