292fde3b688fd9ccb5b2861d1a3f0725e945464d
Emmy <emilyileeva@gmail.com>
2024-04-22 13:56:52 -0700

    add authentication, update navbar, create forms.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 class Animal(models.Model):
     name = models.CharField(max_length=100)
