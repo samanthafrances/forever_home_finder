0f47549875f1845c10b0e9970171efeea802be1a
CoreyCameron <coreycameron26@gmail.com>
2024-04-22 11:31:16 -0400

    edit admin file

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Animal, Blog, Subscriber, Comment, Message, Profile 
 
 # Register your models here.
+admin.site.register(Animal)
+admin.site.register(Blog)
+admin.site.register(Subscriber)
+admin.site.register(Comment)
+admin.site.register(Message)
+admin.site.register(Profile)
